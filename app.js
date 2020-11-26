0e960dcc346999344a512ffa18478a7326d0266c
Daniel <dan20402@gmail.com>
2020-11-26 19:06:26 -0300

    Finish basic pic and info

--- app.js
+++ app.js
@@ -1,48 +1,57 @@
-const display = document.querySelector('#display');
-const list = document.querySelector('.list');
-const listItem = document.querySelector('.listItem');
-const selectList = document.querySelector('#nomes');
+//API dadosabertos do congresso nacional
+const urlDadosAbertos = 'https://dadosabertos.camara.leg.br/api/v2/deputados';
 
-selectList.addEventListener('click', (e) => {
-  console.log(e.target.value);
-});
+fetch(urlDadosAbertos)
+  .then((response) => response.json())
+  .then((data) => {
+    let arrayTodosDeputados = data.dados;
 
-//Funçao para popular o <select>
-function selecionaDeputado(arrayTodosDeputados) {
-  arrayTodosDeputados.forEach((object) => {
-    //Criar <option> para cada nome e um className=opt
-    const option = document.createElement('option');
-    option.innerHTML = `${object.nome}`;
-    option.className = 'opt';
+    const display = document.querySelector('#display');
+    const list = document.querySelector('.list');
+    const selectList = document.querySelector('#nomes');
+    const foto = document.getElementById('foto');
 
-    //Criar atributo value='id'
-    const attributeName = document.createAttribute('value');
-    attributeName.value = `${object.id}`;
-    option.setAttributeNode(attributeName);
-    selectList.appendChild(option);
+    popularSelect(arrayTodosDeputados);
+
+    selectList.addEventListener('click', (e) => {
+      //console.log(e.target.value);
+      deputados(Number(e.target.value));
+    });
+
+    //Funçao para popular o tag select
+    function popularSelect(arrayTodosDeputados) {
+      arrayTodosDeputados.forEach((object) => {
+        //Criar <option> para cada nome e um className=opt
+        const option = document.createElement('option');
+        option.innerHTML = `${object.nome}`;
+        option.className = 'opt';
+
+        //Criar atributo value='id' em <option>
+        const attributeName = document.createAttribute('value');
+        attributeName.value = `${object.id}`;
+        option.setAttributeNode(attributeName);
+        selectList.appendChild(option);
+      });
+    }
+
+    function deputados(id) {
+      //Evita as primeiras 513 NaN do select
+      if (!isNaN(id)) {
+        const deputado = arrayTodosDeputados.filter((obj) => obj.id === id);
+
+        //Apaga a lista do deputado anterior
+        while (list.firstChild) list.removeChild(list.firstChild);
+
+        for (let key in deputado[0]) {
+          if (key === 'nome' || key === 'siglaPartido' || key === 'siglaUf') {
+            const li = document.createElement('li');
+            li.innerHTML = `${key} : ${deputado[0][key]}`;
+            list.appendChild(li);
+          }
+          if (key === 'urlFoto') {
+            foto.setAttribute('src', `${deputado[0][key]}`);
+          }
+        }
+      }
+    }
   });
-}
-
-function deputados(arrayTodosDeputados) {
-  for (const property in arrayTodosDeputados[0]) {
-    console.log(property, arrayTodosDeputados[0][property]);
-    if (
-      property === 'nome' ||
-      property === 'siglaPartido' ||
-      property === 'siglaUf'
-    ) {
-      const li = document.createElement('li');
-      li.innerHTML = `${property} : ${arrayTodosDeputados[0][property]}`;
-      li.className = 'listItem';
-      list.appendChild(li);
-    }
-
-    if (property === 'urlFoto') {
-      const foto = document.createElement('img');
-      const att = document.createAttribute('src');
-      att.value = `${arrayTodosDeputados[0][property]}`;
-      foto.setAttributeNode(att);
-      list.insertAdjacentElement('beforebegin', foto);
-    }
-  }
-}
